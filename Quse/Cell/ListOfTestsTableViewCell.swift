3c6de3b2963ca0161401b1025310fa77857933c1
DNemercev <mikemsc2012dima@gmail.com>
2022-10-17 08:18:41 -0700

    first commit

--- Quse/Cell/ListOfTestsTableViewCell.swift
+++ Quse/Cell/ListOfTestsTableViewCell.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class ListOfTestsTableViewCell: UITableViewCell {
-
-//    let testInfo: TestInfo?
-    
     @IBOutlet weak var categoryImageView: UIImageView!
     @IBOutlet weak var testNameLabel: UILabel!
-    @IBOutlet weak var starsRaitingStack: UIStackView!
+    @IBOutlet var raitingStarsOutleCollection: [UIImageView]!
     
-    
+    let starColor = UIColor(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1))
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
     
+    func showRaiting(raiting: Int) {
+        if (0...5).contains(raiting) {
+            for num in 0...raiting {
+                raitingStarsOutleCollection[num].image = UIImage(systemName: "star.fill")?.withTintColor(starColor, renderingMode: .alwaysOriginal)
+            }
+        }
+    }
+    
+    func configTestCell(raiting: Int, testName: String, groupImage: String) {
+        showRaiting(raiting: raiting)
+        categoryImageView.image = UIImage(systemName: groupImage)
+        testNameLabel.text = testName
+    }
 }
--- Quse/ViewControllers/ListOfTestsViewController.swift
+++ Quse/ViewControllers/ListOfTestsViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class ListOfTestsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var listOfTestsTableView: UITableView!
     override func viewDidLoad() {
+        
+        listOfTestsTableView.delegate = self
+        listOfTestsTableView.dataSource = self
+        
+        listOfTestsTableView.register(UINib(nibName: "ListOfTestsTableViewCell", bundle: nil), forCellReuseIdentifier: "ListOfTestsTableViewCell")
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ListOfTestsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = listOfTestsTableView.dequeueReusableCell(withIdentifier: "ListOfTestsTableViewCell", for: indexPath) as? ListOfTestsTableViewCell {
+            cell.configTestCell(raiting: 3, testName: "Test name", groupImage: "star")
+            return cell
+        }
+        return UITableViewCell()
     }
-    */
-
 }
